a663b33b10ce46121d18928a8e71a75c6c5c2486
AndrewMc8 <78245384+AndrewMc8@users.noreply.github.com>
2022-10-06 14:00:05 -0600

    Room Script Created

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+public class Room
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 gridPosition;
 
-    // Update is called once per frame
-    void Update()
+    public int roomType;
+
+    public bool doorNorth;
+    public bool doorEast;
+    public bool doorSouth;
+    public bool doorWest;
+
+    public Room(Vector2 gridPos, int type)
     {
-        
+        gridPosition = gridPos;
+        roomType = type;
     }
 }
